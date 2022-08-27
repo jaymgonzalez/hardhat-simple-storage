aaa13a4a67b8210bfa3255566d2e90ac834624a2
Jay M Gonzalez <jaimungon@gmail.com>
2022-08-27 17:34:29 +0200

    add programmatic verification

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,9 +1,11 @@
 import { HardhatUserConfig } from 'hardhat/config'
 import '@nomicfoundation/hardhat-toolbox'
+import '@nomiclabs/hardhat-etherscan'
 import 'dotenv/config'
 
 const RINKEBY_RPC_URL = process.env.RINKEBY_RPC_URL!
 const PRIVATE_KEY = process.env.PRIVATE_KEY!
+const ETHERSCAN_API_KEY = process.env.ETHERSCAN_API_KEY
 
 const config: HardhatUserConfig = {
   defaultNetwork: 'hardhat',
@@ -13,6 +15,9 @@ const config: HardhatUserConfig = {
       accounts: [PRIVATE_KEY],
       chainId: 4,
     },
+  },
+  etherscan: {
+    apiKey: ETHERSCAN_API_KEY,
   },
   solidity: '0.8.7',
 }
--- scripts/deploy.ts
+++ scripts/deploy.ts
@@ -1,15 +1,38 @@
-import { ethers } from 'hardhat'
+import { ethers, run, network } from 'hardhat'
 
 async function main() {
   const SimpleStorageFactory = await ethers.getContractFactory('SimpleStorage')
   console.log('Deploying SimpleStorage contract...')
+
   const simpleStorage = await SimpleStorageFactory.deploy()
   await simpleStorage.deployed()
+  if (network.config.chainId === 4 && process.env.ETHERSCAN_API_KEY) {
+    // we want to wait to get the blockchain in sync before verifying
+    console.log('Waiting for block txes...')
+    await simpleStorage.deployTransaction.wait(6)
+    await verify(simpleStorage.address, [])
+  }
   console.log('Simple Storage deployed to:', simpleStorage.address)
 }
 
+async function verify(contractAddress: string, args: any[]) {
+  console.log('Verifying contract...')
+  try {
+    await run('verify:verify', {
+      address: contractAddress,
+      constructorArguments: args,
+    })
+  } catch (err: any) {
+    if (err.message.toLowerCase().includes('already verified')) {
+      console.log('Already verified!')
+    } else {
+      console.log(err)
+    }
+  }
+}
+
 main()
-  .then(() => process.exit(1))
+  .then(() => process.exit(0))
   .catch((err) => {
     console.error(err)
     process.exit(1)
