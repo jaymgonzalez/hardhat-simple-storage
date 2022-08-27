d740d97894126490c06712ec8a00fb4912b880d4
Jay M Gonzalez <jaimungon@gmail.com>
2022-08-27 15:04:22 +0200

    add network rinkeby config

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,7 +1,19 @@
 import { HardhatUserConfig } from 'hardhat/config'
 import '@nomicfoundation/hardhat-toolbox'
+import 'dotenv/config'
+
+const RINKEBY_RPC_URL = process.env.RINKEBY_RPC_URL!
+const PRIVATE_KEY = process.env.PRIVATE_KEY!
 
 const config: HardhatUserConfig = {
+  defaultNetwork: 'hardhat',
+  networks: {
+    rinkeby: {
+      url: RINKEBY_RPC_URL,
+      accounts: [PRIVATE_KEY],
+      chainId: 4,
+    },
+  },
   solidity: '0.8.7',
 }
 
