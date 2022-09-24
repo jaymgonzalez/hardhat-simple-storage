6016b1007bbdcfd607c650c8ae64910af9d100ec
Jay M Gonzalez <jaimungon@gmail.com>
2022-09-24 12:33:23 +0200

    add update call test

--- test/test-deply.ts
+++ test/test-deply.ts
@@ -11,6 +11,15 @@ describe('SimpleStorage', function () {
   it('Should start with a fav number of 0', async function () {
     const currentValue = await simpleStorage.retrieve()
     const expectedValue = '0'
+
+    assert.equal(currentValue.toString(), expectedValue)
+  })
+  it('Should update when we call update', async function () {
+    const expectedValue = '5'
+    const txResponse = await simpleStorage.store(expectedValue)
+    await txResponse.wait(1)
+    const currentValue = await simpleStorage.retrieve()
+
     assert.equal(currentValue.toString(), expectedValue)
   })
 })
