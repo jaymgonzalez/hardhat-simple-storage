f71dd45327a4740baa7e46bcbdb48bc687a56b1d
Jay M Gonzalez <jaimungon@gmail.com>
2022-09-25 19:46:49 +0200

    add gas and coverage

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -4,10 +4,12 @@ import '@nomiclabs/hardhat-etherscan'
 import 'dotenv/config'
 import './tasks/block-number'
 import 'hardhat-gas-reporter'
+import 'solidity-coverage'
 
 const RINKEBY_RPC_URL = process.env.RINKEBY_RPC_URL!
 const PRIVATE_KEY = process.env.PRIVATE_KEY!
 const ETHERSCAN_API_KEY = process.env.ETHERSCAN_API_KEY
+const COINMARKETCAP_API_KEY = process.env.COINMARKETCAP_API_KEY
 
 const config: HardhatUserConfig = {
   defaultNetwork: 'hardhat',
@@ -27,7 +29,12 @@ const config: HardhatUserConfig = {
   },
   solidity: '0.8.7',
   gasReporter: {
-    enabled: true,
+    enabled: false,
+    outputFile: 'gas-report.txt',
+    noColors: true,
+    currency: 'USD',
+    coinmarketcap: COINMARKETCAP_API_KEY,
+    token: 'MATIC',
   },
 }
 
